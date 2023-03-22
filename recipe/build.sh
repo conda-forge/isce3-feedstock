ae1d230ed90c26aca5c3d4cfad780b8f6de6b24e
Ryan Burns <rtburns@jpl.nasa.gov>
2023-03-21 18:01:12 -0700

    Hotfix HDF5 include dirs (#38)

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,11 @@
 set -euo pipefail
 
+# Workaround for leftover build directory paths in HDF5 installation
+# https://github.com/HDFGroup/hdf5/issues/2422
+sed -i.tmp 's/-I.*H5FDsubfiling//' \
+    "$(which h5cc)" \
+    "$(which h5c++)"
+
 mkdir build && cd build
 
 # Build without CUDA support or vendored libs
